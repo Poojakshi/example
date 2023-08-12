0929b969448f900e7f2fae5c0a0f376ee7f2875d
Poojakshi <138793274+Poojakshi@users.noreply.github.com>
2023-08-12 11:54:09 +0530

    Add files via upload

---
+++ employee-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-profile',
+  templateUrl: './employee-profile.component.html',
+  styleUrls: ['./employee-profile.component.css']
+})
+export class EmployeeProfileComponent {
+  user:any;
+  editing:boolean=false;
+}
