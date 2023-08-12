5b003bf804484f701a507a6573d9aaa55200fa8b
Poojakshi <138793274+Poojakshi@users.noreply.github.com>
2023-08-12 11:53:08 +0530

    Add files via upload

---
+++ creator-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creator-profile',
+  templateUrl: './creator-profile.component.html',
+  styleUrls: ['./creator-profile.component.css']
+})
+export class CreatorProfileComponent {
+  user:any;
+  editing:boolean=false;
+}
